7498032afb7fb512d40cd9b6054484261aeef642
Luna Vulperya <lunavulperya@gmail.com>
2026-08-13 12:28:32 -0300

    Fixed kitty revuild issue with settings

--- modules/features/kitty/kitty.nix
+++ modules/features/kitty/kitty.nix
@@ -12,15 +12,15 @@
         confirm_os_window_close = 0;
         window_padding_width = 8;
 
-        keybindings = {
-          "ctrl+c" = "copy_or_interrupt";
-          "ctrl+v" = "paste_from_clipboard";
-        };
-
         # transparency + native compositor blur (kitty 0.46+, niri implements
         # ext-background-effect, so this "just works" on your niri setup)
         background_opacity = "0.75";
         dynamic_background_opacity = true;
+      };
+
+      keybindings = {
+          "ctrl+c" = "copy_or_interrupt";
+          "ctrl+v" = "paste_from_clipboard";
       };
     };
   };

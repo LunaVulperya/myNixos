4c8ea380eb626050180b83ebf8efcf56513822a3
Luna Vulperya <lunavulperya@gmail.com>
2026-08-13 12:19:15 -0300

    Fixed added some fixes to fish and kitty and added starship conf

--- modules/features/fish/fish.nix
+++ modules/features/fish/fish.nix
@@ -11,7 +11,25 @@
         gs = "git status";
       };
       interactiveShellInit = ''
-        set -g fish_greeting
+        # syntax highlighting palette
+        set -g fish_color_command         magenta --bold
+        set -g fish_color_param           cyan
+        set -g fish_color_error           red --bold
+        set -g fish_color_autosuggestion  brblack
+        set -g fish_color_comment         brblack
+
+        function fish_greeting
+          set -l user (whoami)
+          set -l host (hostname)
+          set_color magenta --bold
+          echo -n "  $user"
+          set_color normal
+          echo -n "@"
+          set_color cyan --bold
+          echo -n "$host"
+          set_color normal
+          echo ""
+        end
       '';
     };
   };
--- modules/features/kitty/kitty.nix
+++ modules/features/kitty/kitty.nix
@@ -12,6 +12,11 @@
         confirm_os_window_close = 0;
         window_padding_width = 8;
 
+        keybindings = {
+          "ctrl+c" = "copy_or_interrupt";
+          "ctrl+v" = "paste_from_clipboard";
+        };
+
         # transparency + native compositor blur (kitty 0.46+, niri implements
         # ext-background-effect, so this "just works" on your niri setup)
         background_opacity = "0.75";
